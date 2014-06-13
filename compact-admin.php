ae16d9a34cca5dd39178a827697c420a5291a175
Reimund Trost <reimund@code7.se>
2014-06-13 08:48:43 +0200

    Only load css & script on edit.php with post_type set to 'post' or 'page'.

--- compact-admin.php
+++ compact-admin.php
@@ -4,19 +4,18 @@ Plugin Name: Compact Admin
 Plugin URI: http://lumens.se/compact-admin/
 Description: Makes the post & pages list in the admin more compact. It also replaces the em dashes with whitespace in the pages list for a cleaner look.
 Author: reimund
-Version: 1.1.1
+Version: 1.1.2
 Author URI: http://lumens.se/
 */
 
-function compact_admin_head()
-{
-	echo '<link rel="stylesheet" type="text/css" href="' . plugins_url(basename(dirname(__FILE__))) . '/media/css/compact-admin.css" />';
-}
-
 function admin_scripts() {
-	wp_enqueue_script('compact-admin', plugins_url(basename(dirname(__FILE__)) . '/media/js/compact-admin.js'), array('jquery'));
+	global $pagenow;
+	if ('edit.php' == $pagenow && ('page' == $_GET['post_type'] || 'post' == $_GET['post_type'])) {
+		wp_enqueue_script('compact-admin', plugins_url(basename(dirname(__FILE__)) . '/media/js/compact-admin.js'), array('jquery'));
+		wp_register_style('compact_admin', plugins_url(basename(dirname(__FILE__))) . '/media/css/compact-admin.css', array());
+		wp_enqueue_style('compact_admin');
+	}
 }
 
-add_action('admin_head', 'compact_admin_head');
-add_action( 'admin_print_scripts-edit.php', 'admin_scripts' );
+add_action('admin_print_scripts-edit.php', 'admin_scripts' );
 ?>
